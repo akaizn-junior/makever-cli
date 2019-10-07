7d6e566a3f8538630ca812fda24bb6eb44a9736f
Simao Nziaka <sdnziaka@gmail.com>
2019-10-06 20:27:52 -0600

    new v file

--- src/CLI.js
+++ src/CLI.js
@@ -177,7 +177,11 @@ async function run_npm_version(args) {
         contents.patch = patch;
         contents.branch = branch;
         contents['prerelease'] = prerelease_value;
-        cache_data && prerelease_label in cache_data || prerelease_label.length
+
+        cache_data && prerelease_label.length && prerelease_label in cache_data
+            && (contents[prerelease_label] = true);
+
+        prerelease_label.length
             && (contents[prerelease_label] = true);
 
         // generate version file
