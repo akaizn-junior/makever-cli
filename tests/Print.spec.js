9055c1fe8de5f9099680247cd105544a41657375
Simao Nziaka <sdnziaka@gmail.com>
2019-10-16 20:41:06 -0600

    import print correctly in tests

--- tests/Print.spec.js
+++ tests/Print.spec.js
@@ -1,7 +1,7 @@
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
-const Print = require('../src/Print');
+const Print = require('../src/pretty/Print');
 
 describe('Print module tests', () => {
 	const displayFrequency = 3;
